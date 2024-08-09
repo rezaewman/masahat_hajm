cb665564215ca8860d7dd6ad1bd08da2bce679df
Reza Fattahi <rezaewman2@gmail.com>
2024-08-09 18:26:55 +0430

    masahat_hajm

---
+++ main.py
@@ -0,0 +1,7 @@
+pi = 22 / 7
+height = float(input('Height of cylinder: '))
+radian = float(input('radius of cylinder: '))
+volume = pi * radian * radian * height
+sur_area =((2* pi* radian)* height ) +(( pi * radian ** 2 ) * 2)
+print("Volume is: ", volume)
+print("surface Area is: ", sur_area)
